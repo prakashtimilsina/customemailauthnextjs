a72a0f7d2f987c6e35f7297c639b228d5dc29e9e
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2023-08-03 21:21:35 -0500

    register UI

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -5,7 +5,7 @@ const LoginForm = () => {
   return (
     <div className='grid place-items-center h-screen'>
         <div className='shadow-lg p-5 rounded-lg border-t-4 border-green-400'>
-            <h1 className='text-xl font-bold my-4'>Enter the details</h1>
+            <h1 className='text-xl font-bold my-4'>Login</h1>
             <form className='flex flex-col gap-4'>
                 <input type='text' placeholder='Email' />
                 <input type='password' placeholder='password' />
--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Link from 'next/link';
 
 const RegisterForm = () => {
   return (
-    <div>RegisterForm</div>
+    <div className='grid place-items-center h-screen'>
+        <div className='shadow-lg p-5 rounded-lg border-t-4 border-green-400'>
+            <h1 className='text-xl font-bold my-4'>Register</h1>
+            <form className='flex flex-col gap-4'>
+                <input type='text' placeholder='Full Name' />
+                <input type='text' placeholder='Email' />
+                <input type='password' placeholder='password' />
+                <button className='bg-green-600 text-white font-bold cursor-pointer px-6 py-2 rounded-lg' type='submit'>Register</button>
+
+                <div className='bg-red-400 text-white w-fit text-sm py-1 px-3 rounded-md mt-2'>
+                    Error Message
+                </div>
+                <Link className='text-sm mt-2 text-right' href='/'>
+                    Already have an account? <span className='underline'>
+                        Login
+                    </span>
+                </Link>
+            </form>
+        </div>
+    </div>
   )
 }
 
